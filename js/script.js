60803d2b291209adad2d65f63fd29a6a0607561d
Pierre Gronnier <pierre.gronnier@gmail.com>
2024-12-03 13:51:25 +0100

    add basic operator functions

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let num1 = 5
+let num2 = 2
+let operator = "+";
+
+
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    if (b === 0) {
+        return "Error: Division by zero!";
+    }
+    return a / b;
+};
+
+const operate = function (num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Error: Invalid operator!";
+    }
+};
